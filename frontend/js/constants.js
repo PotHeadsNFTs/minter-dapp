b233b8de566623b77a36418234e5be601c7c5fba
ProllyGlews <83798734+ProllyGlews@users.noreply.github.com>
2022-04-18 18:56:05 -0400

    hi

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome Pot Head!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Pot Head NFT Project is the first of a new kind of NFTs, accessible to all!
+const welcome_p = 'The Pot Head NFT Project is the first of a new kind of NFTs, accessible to all!'
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
