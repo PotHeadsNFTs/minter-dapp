13be2bd1b5e9ef80593e5e4a26659d0343b3d98f
ProllyGlews <83798734+ProllyGlews@users.noreply.github.com>
2022-05-20 14:12:11 -0400

    s

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,7 +7,7 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Pot Heads NFT";
+const namePrefix = "PotHead NFT x 13th";
 const description = "Too high to remember to replace this description";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
@@ -22,7 +22,6 @@ const layerConfigurations = [
       { name: "Faces" },
       { name: "Accessories" },
       { name: "Fills" },
-      { name: "Hats" },
     ],
   },
 ];
@@ -49,25 +48,25 @@ const LIMIT = 10; // Your API key rate limit
 const CHAIN = 'Polygon'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'Pot Heads';
+const CONTRACT_NAME = 'PotHeads x 13th';
 const CONTRACT_SYMBOL = 'POTS';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x6cbD092eE8614eADc1a122DdabF3E0242fbb65Eb';
 const TREASURY_ADDRESS = '0x6cbD092eE8614eADc1a122DdabF3E0242fbb65Eb';
-const MAX_SUPPLY = 123456; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 2; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MAX_SUPPLY = 10000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 5; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 710; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-20T20:20:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-14T07:10:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-19T21:10:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-20T20:20:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x6cbD092eE8614eADc1a122DdabF3E0242fbb65Eb"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0x6cbD092eE8614eADc1a122DdabF3E0242fbb65Eb","0x678CD33aa7988e9aD3dd541697366F629e8477Bc","0x07F159d1A1c886944d0aA751f5374325ff321148"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x6cbD092eE8614eADc1a122DdabF3E0242fbb65Eb","0x678CD33aa7988e9aD3dd541697366F629e8477Bc","0x07F159d1A1c886944d0aA751f5374325ff321148","0xd0C05d7b5a8a236f92F8203EFD90AA9d2EA8baDa","0x6Aa37456e9577BcAa2cb155122b040EDC6D9B300","0xeC4952ee6Ea8B4413228DeFf7Bf6FC201115DB12"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
